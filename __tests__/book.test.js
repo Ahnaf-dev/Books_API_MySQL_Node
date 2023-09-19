c8ac9fe3af5ab9cc6d6b690d2861c6e5dc99ba8b
Ahnaf-dev <ahnafbari99@gmail.com>
2023-09-19 16:05:07 -0600

    Update - test routes

--- __tests__/book.test.js
+++ __tests__/book.test.js
@@ -13,4 +13,33 @@ describe('Book Routes', () => {
     expect(response.body.title).toBe('Test Book');
   });
 
+  it('should retrieve books', async () => {
+    const response = await request(app).get('/api/books');
+
+    expect(Array.isArray(response.body)).toBe(true);
+  });
+
+  it('should update a book by ID', async () => {
+    const createResponse = await request(app)
+      .post('/api/books')
+      .send({ title: 'Update Test Book' });
+
+    const updateResponse = await request(app)
+      .put(`/api/books/${createResponse.body.id}`)
+      .send({ title: 'Updated Book Title' });
+
+    expect(updateResponse.body.id).toBe(`${createResponse.body.id}`);
+    expect(updateResponse.body.title).toBe('Updated Book Title');
+  });
+
+  it('should delete a book by ID', async () => {
+    const createResponse = await request(app)
+      .post('/api/books')
+      .send({ title: 'Update Test Book' });
+
+    const deleteResponse = await request(app).delete(`/api/books/${createResponse.body.id}`);
+
+    expect(deleteResponse.status).toBe(204);
+  });
+  
 })
